9f478177ead600df6b283d3630282d3174b7fca2
Whittelist <bj.azul238@gmail.com>
2025-05-28 08:58:36 +0200

    Activar CORS para el frontend

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const { MongoClient } = require("mongodb");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 const uri = process.env.MONGO_URI;
 const dbName = process.env.DB_NAME;
